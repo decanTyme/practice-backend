acd322e9db86a3f62b2e5b1fd5913d26ca694fd6
Danry Ague <74456102+epcraft93@users.noreply.github.com>
2021-08-17 01:38:56 +0800

    Remove hardcode query [?] from paths

--- routes/items.js
+++ routes/items.js
@@ -5,9 +5,9 @@ const authenticateToken = require("../services/authenticate-token");
 const router = express.Router();
 
 // Item management routes
-router.get("/load?", authenticateToken, itemCtrl.load);
-router.post("/add?", authenticateToken, itemCtrl.addProduct);
+router.get("/load", authenticateToken, itemCtrl.load);
+router.post("/add", authenticateToken, itemCtrl.addProduct);
 router.put("/modify", authenticateToken, itemCtrl.updateProduct);
-router.delete("/del?", authenticateToken, itemCtrl.deleteProduct);
+router.delete("/del", authenticateToken, itemCtrl.deleteProduct);
 
 module.exports = router;
