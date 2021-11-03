39b82dc66564db8e0954575c1d2805cd7fac6bd2
Danry Ague <74456102+epcraft93@users.noreply.github.com>
2021-11-04 01:51:34 +0800

    Updates

--- services/db-check.js
+++ services/db-check.js
@@ -4,13 +4,17 @@ async function checkDbConnection(req, res, next) {
   // Mongoose connect flag
   await db
     .then(() => {
-      console.log("Database connected");
+      console.log(`[${new Date().toLocaleString()}]`, "Database connected");
       req.isDbConnected = true;
     })
     .catch((error) => {
       req.isDbConnected = false;
       req.databaseConnectError = error;
-      console.error("Database connect error!", error);
+      console.error(
+        `[${new Date().toLocaleString()}]`,
+        "Database connect error!",
+        error
+      );
     });
 
   next();
